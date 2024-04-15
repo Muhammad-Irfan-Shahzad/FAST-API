9e0e23d0520f8a9d3c1123b376f264e1c7c4cd32
Muhammad-Irfan-Shahzad <nnsirfanshah@gmail.com>
2024-04-15 09:58:39 +0500

    mdoel + api complete code

---
+++ flan.py
@@ -0,0 +1,22 @@
+from huggingface_hub import HfApi, HfFolder
+
+TOKEN = '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'
+
+api = HfApi()
+api.token = TOKEN
+
+models = api.list_models()
+# print(models)
+
+import warnings
+warnings.filterwarnings('ignore') 
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+def fun(t):
+    model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+
+    inputs = tokenizer("Provide information on this [" +str(t)+ "]", return_tensors="pt")
+    outputs = model.generate(**inputs)
+    v = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return v
